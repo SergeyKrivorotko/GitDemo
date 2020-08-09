2c3a9f4d26f70c1b5f95839e9a9e3eeb77f8133f
Admin <admin@Mac-Admin.local>
2020-08-09 13:04:01 +0300

    initial commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Admin on 01.07.20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        cenliusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+    }
+    
+    
+}
+
